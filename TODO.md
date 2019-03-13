3eb13dbc5e157ac7a6e88475e8f4922ae044cf84
Bigaston <bigaston1@gmail.com>
2019-03-13 10:18:23 +0100

    Remove web and add lang

--- TODO.md
+++ TODO.md
@@ -1,12 +1,7 @@
-# Website
-- Discord login
-- Join server
-- Write JSON code and send to DB
-- Visual Scripting
-
 # Discord
 - Command to add command
 - Modify option in Discord
+- Add hardcoder command (info, script)
 
 # Synthax
 - Add action (add roles, kick member)
@@ -17,6 +12,13 @@
 - Add some tracery grammar (?)
 - Event to trigger at a time
 - Params with date/hours/min/seconds
+- Option to change the game of the bot
+- Add option to search in the lang file
+- Option to test the channel
+- Add option to other channel
 
 # Other
-- Upgrade doc
+- Upgrade doc (@token@, lang file)
+- Add a script.json demo
+- Add verification at start with needeed params
+- Add log in console
--- easy.js
+++ easy.js
@@ -1,59 +1,26 @@
-const config = require("./config.json")
-
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-
-const adapter = new FileSync('db.json')
-const db = low(adapter)
-
-var express = require('express');
-var app = express();
-
-var bodyParser = require('body-parser')
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-app.set('view engine', 'pug');
-app.set('views', './views')
-app.use(express.static("./static/"))
+var script = require("./script.json");
+const lang = require("./lang.json");
 
 var version = "0.3.0";
 
-db.defaults({ serveur : [] })
-  .write()
-
-client.login(config.token);
+client.login(script["@token@"]);
 
 client.on("ready", async () => {
 	client.user.setActivity(version);
-	verifyGuilds();
 
-	var serveurList = db.get("serveur").value()
-
-	for (var i=0; i < serveurList.length; i++) {
-		serveurObj = client.guilds.get(serveurList[i].id);
+	var clientGuild = client.guilds.array();
+	for (var i=0; i < clientGuild.length; i++) {
+		serveurObj = client.guilds.get(clientGuild[i].id);
 		changeNickname(serveurObj);
 	}
 });
 
-function verifyGuilds() {
-	// Detect if serveur is in the API or not
-	var clientGuild = client.guilds.array();
-
-	for (var i=0; i < clientGuild.length; i++) {
-		if (db.get("serveur").find({ id: clientGuild[i].id}).value() == undefined) {
-			db.get("serveur").push({ id: clientGuild[i].id, code: {"@prefix@": "!"}}).write()
-		}
-	}
-}
-
 function changeNickname(pServeur) {
 	// Detect if the bot username is different
-	var script = db.get("serveur").find({ id: pServeur.id}).value().code;
+
 	if ("@customName@" in script) {
 		if (pServeur.me.displayName != script["@customName@"]) {
 			pServeur.me.setNickname(script["@customName@"]);
@@ -121,7 +88,6 @@ function clone(obj) {
 
 client.on("message", message => {
 	// Commands
-	var script = db.get("serveur").find({ id: message.guild.id}).value().code;
 	if(message.author.bot) return;
 	if (message.content.startsWith(script["@prefix@"])) {
 		args = message.content.split(/[ ]+/);
@@ -175,7 +141,6 @@ client.on("message", message => {
 
 client.on("guildMemberAdd", user => {
 	// Detect user join
-	var script = db.get("serveur").find({ id: user.guild.id}).value().code;
 	if ("#userJoin#" in script) {
 		var text = script["#userJoin#"]
 		text = text.replace("%user%", `<@${user.id}>`)
@@ -186,7 +151,6 @@ client.on("guildMemberAdd", user => {
 
 client.on("guildMemberRemove", user => {
 	// Detect user leave
-	var script = db.get("serveur").find({ id: user.guild.id}).value().code;
 	if ("#userLeave#" in script) {
 		var text = script["#userLeave#"]
 		text = text.replace("%user%", user.displayName)
@@ -197,22 +161,11 @@ client.on("guildMemberRemove", user => {
 
 client.on("guildCreate", guild => {
 	//Detect a new Discord server
-	if (db.get("serveur").find({ id: guild.id}).value() == undefined) {
-		db.get("serveur").push({ id: guild.id, code: {"@prefix@": "!"}}).write()
+	if ("@infoChannel@" in script) {
+		if ("@lang@" in script) {
+			client.channels.get(script["@infoChannel@"]).send(lang[script["@lang@"]].join);
+		} else {
+			client.channels.get(script["@infoChannel@"]).send(lang["en"].join);
+		}
 	}
 })
-
-client.on("guildDelete", guild => {
-	//Detect a new Discord server
-	db.get("serveur")
-  		.remove({ id: guild.id })
-  		.write()
-})
-
-app.listen(config.port, function() {
-	console.log("Démarage du serveur sur le port " + config.port)
-});
-
-app.get('/discord_login', function(req, res) {
-	
-});
